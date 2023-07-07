079aa843c3fcbefd12c0228e752a1b107c6d2e53
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-07 00:10:10 -0700

    implement NavBar

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,10 @@
+import { NavBar } from '../../components/NavBar';
 import './App.css';
 
 export default function App() {
   return (
     <main className="App">
+      <NavBar />
       App
     </main>
   );

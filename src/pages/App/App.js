354a6969c02548443636a74100f008f4a703f4f0
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-07 01:48:09 -0700

    implements hero component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,12 @@
 import { NavBar } from '../../components/NavBar';
+import { Hero } from '../../components/Hero';
 import './App.css';
 
 export default function App() {
   return (
     <main className="App">
       <NavBar />
-      App
+      <Hero />
     </main>
   );
 }

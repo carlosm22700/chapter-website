f0dc97f57b0b28d6e59744187afcd77d3883659b
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-07 03:08:25 -0700

    implements Button component from react-bootstrap, updates hero text

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,4 +1,5 @@
 import heroImg from '../../assets/images/hero.jpg'
+import { HeroButton } from '../../components/HeroButton'
 import './HeroStyles.css'
 
 export const Hero = () => {
@@ -7,8 +8,10 @@ export const Hero = () => {
             <img alt='HeroImg' src={heroImg}/>
             <div className='hero-text'>
                 <h1>Beta Psi Chapter</h1>
-                <p><strong>of La Unidad Latina, Lambda Upislon Lambda Fraternity, Incorporated</strong></p>
-                <a href='/'>Learn More</a>
+                {/* <h2>La Unidad Latina, Lambda Upislon Lambda Fraternity, Incorporated</h2> */}
+                <h2>The University of California, Los Angeles</h2>
+                <p><strong>Est. 2021</strong></p>
+                <HeroButton />
             </div>
         </div>
     )

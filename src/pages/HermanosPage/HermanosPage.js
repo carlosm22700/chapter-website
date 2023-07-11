e7488e21121d6996bd85c7efbad88459119d4e45
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-07-10 23:51:48 -0700

    adds card component and ListGroup component

--- src/pages/HermanosPage/HermanosPage.js
+++ src/pages/HermanosPage/HermanosPage.js
@@ -1,85 +1,76 @@
-import Figure from 'react-bootstrap/Figure';
-import headshot from '../../assets/images/headshot.jpg'
-import { Col, Row, Container } from 'react-bootstrap';
+import { ListGroup, Card } from "react-bootstrap";
+import headshot from "../../assets/images/headshot.jpg";
+import { Col, Row, Container } from "react-bootstrap";
 
 export const HermanosPage = () => {
-    return (
-        <main>
-            <h1>Hermanos Page</h1>
-            <div className="undergrads-section">
-                <h2>Undergraduate Hermanos</h2>
-                <Container>
-                <Row>
-                    <Col>
-                        <Figure>
-                            <Figure.Image
-                                width={171}
-                                height={180}
-                                alt="171x180"
-                                src={headshot}
-                            />
-                            <Figure.Caption>
-                                Nulla vitae elit libero, a pharetra augue mollis interdum.
-                            </Figure.Caption>
-                        </Figure>
-                    </Col>
-                    <Col>
-                        <Figure>
-                            <Figure.Image
-                                width={171}
-                                height={180}
-                                alt="171x180"
-                                src={headshot}
-                            />
-                            <Figure.Caption>
-                                Nulla vitae elit libero, a pharetra augue mollis interdum.
-                            </Figure.Caption>
-                        </Figure>
-                    </Col>
-                    <Col>
-                        <Figure>
-                            <Figure.Image
-                                width={171}
-                                height={180}
-                                alt="171x180"
-                                src={headshot}
-                            />
-                            <Figure.Caption>
-                                Nulla vitae elit libero, a pharetra augue mollis interdum.
-                            </Figure.Caption>
-                        </Figure>
-                    </Col>
-                </Row>
-                </Container>
-            </div>
-            <div className="members-section">
-                <h2>Chapter Members</h2>
-                <h3>Alpha Line</h3>
-                    <ul>
-                        <p>Carlos Gomez Guzman</p>
-                        <p>Rene Emanuel Calzadilla</p>
-                        <p>Carlos Steven Martinez</p>
-                        <p>Douglas Alberto Moreno</p>
-                        <p>Alejandro Salas-Villa</p>
-                        <p>Carlos Lionel Torres</p>
-                        <p>Rogelio Ambriz</p>
-                        <p>Abraham Josue Martinez</p>
-                        <p>Daniel Alvarado</p>
-                        <p>Marcelooooooo</p>
-                    </ul>
-                <h3>Beta Line</h3>
-                    <ul>
-                        <p>Lefter</p>
-                        <p>Javier</p>
-                        <p>Assael</p>
-                        <p>Jonathan</p>
-                        <p>Manuel</p>
-                        <p>Luis</p>
-                    </ul>
-            </div>
-        </main>
-    )
-}
+  const members = [
+    {
+      lineName: "Alpha Line",
+      members: [
+        "Lorem Ipsum",
+        "Lorem Ipsum",
+        // Add more names here.
+      ],
+    },
+    {
+      lineName: "Beta Line",
+      members: [
+        "Lorem Ipsum",
+        "Lorem Ipsum",
+        // Add more names here.
+      ],
+    },
+    // Add more lines here.
+  ];
+
+  const undergrads = [
+    {
+      src: headshot,
+      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      src: headshot,
+      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      src: headshot,
+      caption: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+  ];
+  return (
+    <main>
+      <h1>Hermanos Page</h1>
+      <div className="undergrads-section">
+        <h2>Undergraduate Hermanos</h2>
+        <Container>
+          <Row>
+            {undergrads.map((undergrad, index) => (
+              <Col key={index}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={undergrad.src} />
+                  <Card.Body>
+                    <Card.Title>{undergrad.name}</Card.Title>
+                    <Card.Text>{undergrad.caption}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </div>
+      {members.map((line, index) => (
+        <div className="members-section" key={index}>
+          <h2>{line.lineName}</h2>
+          <ListGroup>
+            {line.members.map((member, index) => (
+              <ListGroup.Item key={index}>{member}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+      ))}
+    </main>
+  );
+};
 
 /*
 TODO:
@@ -110,4 +101,32 @@ function AutoLayoutExample() {
 }
 
 export default AutoLayoutExample;
+
+
+
+<div className="members-section" key={index}>
+                <h2>Chapter Members</h2>
+                <h3>Alpha Line</h3>
+                    <ul>
+                        <p>Carlos Gomez Guzman</p>
+                        <p>Rene Emanuel Calzadilla</p>
+                        <p>Carlos Steven Martinez</p>
+                        <p>Douglas Alberto Moreno</p>
+                        <p>Alejandro Salas-Villa</p>
+                        <p>Carlos Lionel Torres</p>
+                        <p>Rogelio Ambriz</p>
+                        <p>Abraham Josue Martinez</p>
+                        <p>Daniel Alvarado</p>
+                        <p>Marcelooooooo</p>
+                    </ul>
+                <h3>Beta Line</h3>
+                    <ul>
+                        <p>Lefter</p>
+                        <p>Javier</p>
+                        <p>Assael</p>
+                        <p>Jonathan</p>
+                        <p>Manuel</p>
+                        <p>Luis</p>
+                    </ul>
+            </div>
 */

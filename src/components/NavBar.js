6f87f0fca575c0e8680fcf3e61cd9acffc4b3cbe
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-08-15 23:52:43 -0700

    add social icon on nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import './NavBarStyles.css';
-import crest from '../assets/images/crest.png';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./NavBarStyles.css";
+import crest from "../assets/images/crest.png";
+import navIcon from "../assets/images/nav-icon3.svg";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <Navbar expand="lg" className="custom-navbar">
       <Container>
         <Navbar.Brand href="/">
-          <img src={crest} alt='Crest' className='navbar-crest' />
+          <img src={crest} alt="Crest" className="navbar-crest" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="align-items-lg-center">
-            <Nav.Link href="#home">Instagram</Nav.Link>
+            <Nav.Link
+              href="https://www.instagram.com/lul_bruins/"
+              target="_blank"
+            >
+              <img src={navIcon} alt="" />
+            </Nav.Link>
             <NavDropdown title="About Us" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -29,13 +35,13 @@ export const NavBar = () => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href='/hermanos'>Hermanos</Nav.Link>
+            <Nav.Link href="/hermanos">Hermanos</Nav.Link>
             <Nav.Link href="#home">Contact Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
-// Use Link from react-router-dom instead of Nav.Link? 
+// Use Link from react-router-dom instead of Nav.Link?

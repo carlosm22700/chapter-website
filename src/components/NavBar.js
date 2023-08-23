c40254ce9ba42fdecfb5d68d54e68cda25836d10
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-08-23 00:52:39 -0700

    remove dropdown from nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,17 +24,7 @@ export const NavBar = () => {
             >
               <img src={navIcon} alt="" />
             </Nav.Link>
-            <NavDropdown title="About Us" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="/about">About Us</Nav.Link>
             <Nav.Link href="/hermanos">Hermanos</Nav.Link>
             <Nav.Link href="#home">Contact Us</Nav.Link>
           </Nav>

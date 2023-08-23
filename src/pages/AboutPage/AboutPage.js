43912037ddb007b8611a7e387d06092e788fe49e
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-08-23 00:53:00 -0700

    create aboutpage and set up routing

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,5 +1,44 @@
 export const AboutPage = () => {
-    return (
-        <h1>About Us</h1>
-    )
-}
+  return (
+    <div>
+      <section className="hero">
+        <h1>About La Unidad Latina</h1>
+        {/* Optionally include a background image, video, or other visuals */}
+      </section>
+
+      <section className="about-story">
+        <h2>Our Story</h2>
+        <p>
+          {/* Brief narrative about the organization's history and mission */}
+        </p>
+      </section>
+
+      <section className="facts-milestones">
+        <h2>Quick Facts & Milestones</h2>
+        {/* List of key details, achievements, and notable milestones */}
+      </section>
+
+      <section className="pillars-values">
+        <h2>Our Core Values</h2>
+        {/* Detailed information on the organization's core principles, values, or initiatives */}
+      </section>
+
+      <section className="awards-recognitions">
+        <h2>Awards & Recognitions</h2>
+        {/* List of awards and accolades received by the organization */}
+      </section>
+
+      <section className="founding-history">
+        <h2>Founding History</h2>
+        <p>
+          {/* Detailed story on the establishment and foundation of the organization */}
+        </p>
+      </section>
+
+      <section className="faq">
+        <h2>Frequently Asked Questions</h2>
+        {/* List of questions and answers, possibly in an accordion format */}
+      </section>
+    </div>
+  );
+};
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import { NavBar } from '../../components/NavBar';
-import { LandingPage } from '../LandingPage/LandingPage';
-import { HermanosPage } from '../HermanosPage/HermanosPage';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "../../components/NavBar";
+import { LandingPage } from "../LandingPage/LandingPage";
+import { HermanosPage } from "../HermanosPage/HermanosPage";
+import { AboutPage } from "../AboutPage/AboutPage";
+import "./App.css";
 
 export default function App() {
   return (
@@ -10,8 +11,9 @@ export default function App() {
       <>
         <NavBar />
         <Routes>
-          <Route path='/' element={<LandingPage /> } />
-          <Route path='/hermanos' element={<HermanosPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/hermanos" element={<HermanosPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </>
     </main>

3e4fc3a1939fd58cc41c1e982a6acf78e64acec9
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-08-24 23:01:28 -0700

    wip: add content for our story section

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -9,9 +9,32 @@ export const AboutPage = () => {
       </section>
 
       <section className="about-story">
-        <h2>Our Story</h2>
+        <h2 className="story-title">Our Story</h2>
+        <h2>The Birth of a Legacy: La Unidad Latina</h2>
         <p>
-          {/* Brief narrative about the organization's history and mission */}
+          In the snow-covered grounds of Ithaca, New York, a transformative idea
+          took root. On Februaty 19, 1982, within the halls of Cornell
+          University, La Unidad Latina, Lambda Upsilon Lambda Fraternity, Inc.
+          came into existence. It's inception was not an accient; it was a
+          calculated answer to a void—a need for representation and advocacy for
+          Latino students in higher education.
+        </p>
+        <h2>A New Horizon: Beta Psi at UCLA</h2>
+        <p>
+          At the University of California, Los Angeles, where Latinos constitue
+          less than 30% of the student body, the need for empowering communities
+          and creating inclusive environments has never been more critical.
+          Thus, in the Summer of 2018, like-minded individuals embarked on a
+          journey. Their mission was to reach out to Latino males on campus,
+          igniting conversations, sparking friendships, and most importantly,
+          identifying allies. The notion to establish a chapter of La Unidad
+          Latina, Lambda Upsilon Lambda Fraternity, Inc. at UCLA was fueld by
+          more than ambition. It was propelled by the ongoing challenges and
+          aspirations of Latino students on campus. After years of unwavering
+          dedication, on May 9th, 2021, the Beta Psi chapter of La Unidad
+          Latina, Lambda Upsilon Lambda Fraternity, Inc. was officially founded
+          at UCLA. Ten pioneering Hermanos, driven by vision and guided by
+          fraternity's legacy, made this aspiration a living reality.
         </p>
       </section>
 

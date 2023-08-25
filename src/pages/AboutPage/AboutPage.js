1ce07e7a0f8450c4ba1a8577388183119c0e82c5
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-08-25 00:55:28 -0700

    wip: about page content

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,5 +1,8 @@
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import { AboutBanner } from "../../components/AboutBanner";
 import "./AboutPageStyles.css";
+import nyImg from "../../assets/images/cornell.jpg";
 
 export const AboutPage = () => {
   return (
@@ -10,32 +13,60 @@ export const AboutPage = () => {
 
       <section className="about-story">
         <h2 className="story-title">Our Story</h2>
-        <h2>The Birth of a Legacy: La Unidad Latina</h2>
-        <p>
-          In the snow-covered grounds of Ithaca, New York, a transformative idea
-          took root. On Februaty 19, 1982, within the halls of Cornell
-          University, La Unidad Latina, Lambda Upsilon Lambda Fraternity, Inc.
-          came into existence. It's inception was not an accient; it was a
-          calculated answer to a void—a need for representation and advocacy for
-          Latino students in higher education.
-        </p>
-        <h2>A New Horizon: Beta Psi at UCLA</h2>
-        <p>
-          At the University of California, Los Angeles, where Latinos constitue
-          less than 30% of the student body, the need for empowering communities
-          and creating inclusive environments has never been more critical.
-          Thus, in the Summer of 2018, like-minded individuals embarked on a
-          journey. Their mission was to reach out to Latino males on campus,
-          igniting conversations, sparking friendships, and most importantly,
-          identifying allies. The notion to establish a chapter of La Unidad
-          Latina, Lambda Upsilon Lambda Fraternity, Inc. at UCLA was fueld by
-          more than ambition. It was propelled by the ongoing challenges and
-          aspirations of Latino students on campus. After years of unwavering
-          dedication, on May 9th, 2021, the Beta Psi chapter of La Unidad
-          Latina, Lambda Upsilon Lambda Fraternity, Inc. was officially founded
-          at UCLA. Ten pioneering Hermanos, driven by vision and guided by
-          fraternity's legacy, made this aspiration a living reality.
-        </p>
+        <div className="story-content-section">
+          <Container>
+            <h2>The Birth of a Legacy: La Unidad Latina</h2>
+            <Row className="flex-container">
+              <Col sm={8} className="flex-item">
+                <p>
+                  In the snow-covered grounds of Ithaca, New York, a
+                  transformative idea took root. On Februaty 19, 1982, within
+                  the halls of Cornell University, La Unidad Latina, Lambda
+                  Upsilon Lambda Fraternity, Inc. came into existence. It's
+                  inception was not an accient; it was a calculated answer to a
+                  void—a need for representation and advocacy for Latino
+                  students in higher education.
+                </p>
+              </Col>
+              <Col sm={4} className="flex-item">
+                <img className="ny-img" src={nyImg} />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="story-content-section">
+          <Container>
+            <h2>A New Horizon: Beta Psi at UCLA</h2>
+            <Row className="flex-container">
+              <Col sm={4} className="flex-item">
+                <img className="ny-img" src={nyImg} />
+              </Col>
+              <Col sm={8} className="flex-item">
+                <p>
+                  At the University of California, Los Angeles, where Latinos
+                  constitue less than 30% of the student body, the need for
+                  empowering communities and creating inclusive environments has
+                  never been more critical.
+                  <br />
+                  Thus, in the Summer of 2018, like-minded individuals embarked
+                  on a journey. Their mission was to reach out to Latino males
+                  on campus, igniting conversations, sparking friendships, and
+                  most importantly, identifying allies. The notion to establish
+                  a chapter of La Unidad Latina, Lambda Upsilon Lambda
+                  Fraternity, Inc. at UCLA was fueld by more than ambition. It
+                  was propelled by the ongoing challenges and aspirations of
+                  Latino students on campus.
+                  <br />
+                  After years of unwavering dedication, on May 9th, 2021, the
+                  Beta Psi chapter of La Unidad Latina, Lambda Upsilon Lambda
+                  Fraternity, Inc. was officially founded at UCLA. Ten
+                  pioneering Hermanos, driven by vision and guided by
+                  fraternity's legacy, made this aspiration a living reality.
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </section>
 
       <section className="facts-milestones">
